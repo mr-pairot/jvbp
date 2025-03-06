2bec9232326fb4abdc4395ec3dff37327a686cd3
mr-pairot <mr.pairot@gmail.com>
2025-03-06 15:24:29 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+ <iframe id="myframe" class="responsive-iframe" src="" allowFullScreen></iframe>
+ <script>
+    const apps = new URL(window.location);
+    const formUrl = apps.searchParams.get('apps');
+    const url = decodeURIComponent(formUrl)
+          document.getElementById('myframe').src = url
+  </script>
